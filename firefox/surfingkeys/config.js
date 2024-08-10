499739f160988153cd52bac6d4bbedbd1fbe270f
William Blazer <william0blazer@gmail.com>
2024-08-10 12:51:14 -0500

    Add gitconfig and firefox customizations

---
+++ firefox/surfingkeys/config.js
@@ -0,0 +1,419 @@
+// A very tridactyl-esque config file.
+
+// Compatibility Prefix
+const {
+  Clipboard,
+  Front,
+  Hints,
+  Normal,
+  RUNTIME,
+  Visual,
+  aceVimMap,
+  addSearchAlias,
+  cmap,
+  getClickableElements,
+  imap,
+  imapkey,
+  iunmap,
+  map,
+  mapkey,
+  readText,
+  removeSearchAlias,
+  tabOpenLink,
+  unmap,
+  unmapAllExcept,
+  vmapkey,
+  vunmap
+} = api;
+
+// ---- Settings ----
+settings.defaultSearchEngine = 'd';
+settings.hintAlign = 'left';
+settings.omnibarPosition = 'bottom';
+settings.focusFirstCandidate = false;
+settings.focusAfterClosed = 'last';
+settings.scrollStepSize = 140;
+settings.tabsThreshold = 0;
+settings.modeAfterYank = 'Normal';
+
+// --- Nav ---
+// Open Clipboard URL in current tab
+mapkey('p', "Open the clipboard's URL in the current tab", () => { Clipboard.read(function(response) { window.location.href = response.data; }); });
+
+// Open Clipboard URL in new tab
+map('P', 'cc');
+
+// History Back/Forward
+map('H', 'S');
+map('L', 'D');
+
+// Scroll Page Down/Up
+mapkey("<Ctrl-d>", "Scroll down", () => { Normal.scroll("pageDown"); });
+mapkey("<Ctrl-u>", "Scroll up", () => { Normal.scroll("pageUp"); });
+
+// --- Tabs ---
+mapkey('u', '#3Restore closed tab', () => { RUNTIME("openLast"); });
+
+// Move Tab Left/Right w/ one press
+map('>', '>>');
+map('<', '<<');
+
+// ---- Unmap -----
+unmap('og');
+unmap('oy');
+
+// ---- Search Engines -----
+removeSearchAlias('b', 's');
+removeSearchAlias('d', 's');
+removeSearchAlias('e', 's');
+removeSearchAlias('h', 's');
+removeSearchAlias('s', 's');
+removeSearchAlias('w', 's');
+
+addSearchAlias('ap', 'arch pkg', 'https://www.archlinux.org/packages/?sort=&q=', 's');
+addSearchAlias('aur', 'aur', 'https://aur.archlinux.org/packages/?O=0&SeB=nd&K=', 's');
+addSearchAlias('aw', 'arch wiki', 'https://wiki.archlinux.org/index.php?title=Special:Search&search=', 's');
+addSearchAlias('r', 'reddit', 'https://libreddit.spike.codes/r/', 's');
+
+// ---- THEME ----
+// Tomorrow-Night
+/* -- DELETE LINE TO ENABLE THEME
+Hints.style('border: solid 2px #373B41; color:#52C196; background: initial; background-color: #1D1F21;');
+Hints.style("border: solid 2px #373B41 !important; padding: 1px !important; color: #C5C8C6 !important; background: #1D1F21 !important;", "text");
+Visual.style('marks', 'background-color: #52C19699;');
+Visual.style('cursor', 'background-color: #81A2BE;');
+-- DELETE LINE TO ENABLE THEME */
+
+// Nord
+Hints.style('border: solid 2px #4C566A; color:#A3BE8C; background: initial; background-color: #3B4252;');
+Hints.style("border: solid 2px #4C566A !important; padding: 1px !important; color: #E5E9F0 !important; background: #3B4252 !important;", "text");
+Visual.style('marks', 'background-color: #A3BE8C99;');
+Visual.style('cursor', 'background-color: #88C0D0;');
+
+// Doom One
+/* -- DELETE LINE TO ENABLE THEME
+Hints.style('border: solid 2px #282C34; color:#98be65; background: initial; background-color: #2E3440;');
+Hints.style("border: solid 2px #282C34 !important; padding: 1px !important; color: #51AFEF !important; background: #2E3440 !important;", "text");
+Visual.style('marks', 'background-color: #98be6599;');
+Visual.style('cursor', 'background-color: #51AFEF;');
+-- DELETE LINE TO ENABLE THEME */
+
+// Monokai
+/* -- DELETE LINE TO ENABLE THEME
+Hints.style('border: solid 2px #2D2E2E; color:#F92660; background: initial; background-color: #272822;');
+Hints.style("border: solid 2px #2D2E2E !important; padding: 1px !important; color: #A6E22E !important; background: #272822 !important;", "text");
+Visual.style('marks', 'background-color: #A6E22E99;');
+Visual.style('cursor', 'background-color: #F92660;');
+-- DELETE LINE TO ENABLE THEME */
+
+settings.theme = `
+/* Edit these variables for easy theme making */
+:root {
+  /* Font */
+  --font: 'Source Code Pro', Ubuntu, sans;
+  --font-size: 12;
+  --font-weight: bold;
+
+  /* -------------- */
+  /* --- THEMES --- */
+  /* -------------- */
+
+  /* -------------------- */
+  /* -- Tomorrow Night -- */
+  /* -------------------- */
+  /* -- DELETE LINE TO ENABLE THEME
+  --fg: #C5C8C6;
+  --bg: #282A2E;
+  --bg-dark: #1D1F21;
+  --border: #373b41;
+  --main-fg: #81A2BE;
+  --accent-fg: #52C196;
+  --info-fg: #AC7BBA;
+  --select: #585858;
+  -- DELETE LINE TO ENABLE THEME */
+
+  /* Unused Alternate Colors */
+  /* --cyan: #4CB3BC; */
+  /* --orange: #DE935F; */
+  /* --red: #CC6666; */
+  /* --yellow: #CBCA77; */
+
+  /* -------------------- */
+  /* --      NORD      -- */
+  /* -------------------- */
+  --fg: #E5E9F0;
+  --bg: #3B4252;
+  --bg-dark: #2E3440;
+  --border: #4C566A;
+  --main-fg: #88C0D0;
+  --accent-fg: #A3BE8C;
+  --info-fg: #5E81AC;
+  --select: #4C566A;
+
+  /* Unused Alternate Colors */
+  /* --orange: #D08770; */
+  /* --red: #BF616A; */
+  /* --yellow: #EBCB8B; */
+
+  /* -------------------- */
+  /* --    DOOM ONE    -- */
+  /* -------------------- */
+  /* -- DELETE LINE TO ENABLE THEME
+  --fg: #51AFEF;
+  --bg: #2E3440;
+  --bg-dark: #21242B;
+  --border: #2257A0;
+  --main-fg: #51AFEF;
+  --accent-fg: #98be65;
+  --info-fg: #C678DD;
+  --select: #4C566A;
+  -- DELETE LINE TO ENABLE THEME */
+
+  /* Unused Alternate Colors */
+  /* --border-alt: #282C34; */
+  /* --cyan: #46D9FF; */
+  /* --orange: #DA8548; */
+  /* --red: #FF6C6B; */
+  /* --yellow: #ECBE7B; */
+
+  /* -------------------- */
+  /* --    MONOKAI    -- */
+  /* -------------------- */
+  /* -- DELETE LINE TO ENABLE THEME
+  --fg: #F8F8F2;
+  --bg: #272822;
+  --bg-dark: #1D1E19;
+  --border: #2D2E2E;
+  --main-fg: #F92660;
+  --accent-fg: #E6DB74;
+  --info-fg: #A6E22E;
+  --select: #556172;
+  -- DELETE LINE TO ENABLE THEME */
+
+  /* Unused Alternate Colors */
+  /* --red: #E74C3C; */
+  /* --orange: #FD971F; */
+  /* --blue: #268BD2; */
+  /* --violet: #9C91E4; */
+  /* --cyan: #66D9EF; */
+}
+
+/* ---------- Generic ---------- */
+.sk_theme {
+background: var(--bg);
+color: var(--fg);
+  background-color: var(--bg);
+  border-color: var(--border);
+  font-family: var(--font);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
+}
+
+input {
+  font-family: var(--font);
+  font-weight: var(--font-weight);
+}
+
+.sk_theme tbody {
+  color: var(--fg);
+}
+
+.sk_theme input {
+  color: var(--fg);
+}
+
+/* Hints */
+#sk_hints .begin {
+  color: var(--accent-fg) !important;
+}
+
+#sk_tabs .sk_tab {
+  background: var(--bg-dark);
+  border: 1px solid var(--border);
+}
+
+#sk_tabs .sk_tab_title {
+  color: var(--fg);
+}
+
+#sk_tabs .sk_tab_url {
+  color: var(--main-fg);
+}
+
+#sk_tabs .sk_tab_hint {
+  background: var(--bg);
+  border: 1px solid var(--border);
+  color: var(--accent-fg);
+}
+
+.sk_theme #sk_frame {
+  background: var(--bg);
+  opacity: 0.2;
+  color: var(--accent-fg);
+}
+
+/* ---------- Omnibar ---------- */
+/* Uncomment this and use settings.omnibarPosition = 'bottom' for Pentadactyl/Tridactyl style bottom bar */
+/* .sk_theme#sk_omnibar {
+  width: 100%;
+  left: 0;
+} */
+
+.sk_theme .title {
+  color: var(--accent-fg);
+}
+
+.sk_theme .url {
+  color: var(--main-fg);
+}
+
+.sk_theme .annotation {
+  color: var(--accent-fg);
+}
+
+.sk_theme .omnibar_highlight {
+  color: var(--accent-fg);
+}
+
+.sk_theme .omnibar_timestamp {
+  color: var(--info-fg);
+}
+
+.sk_theme .omnibar_visitcount {
+  color: var(--accent-fg);
+}
+
+.sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
+  background: var(--bg-dark);
+}
+
+.sk_theme #sk_omnibarSearchResult ul li.focused {
+  background: var(--border);
+}
+
+.sk_theme #sk_omnibarSearchArea {
+  border-top-color: var(--border);
+  border-bottom-color: var(--border);
+}
+
+.sk_theme #sk_omnibarSearchArea input,
+.sk_theme #sk_omnibarSearchArea span {
+  font-size: var(--font-size);
+}
+
+.sk_theme .separator {
+  color: var(--accent-fg);
+}
+
+/* ---------- Popup Notification Banner ---------- */
+#sk_banner {
+  font-family: var(--font);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
+  background: var(--bg);
+  border-color: var(--border);
+  color: var(--fg);
+  opacity: 0.9;
+}
+
+/* ---------- Popup Keys ---------- */
+#sk_keystroke {
+  background-color: var(--bg);
+}
+
+.sk_theme kbd .candidates {
+  color: var(--info-fg);
+}
+
+.sk_theme span.annotation {
+  color: var(--accent-fg);
+}
+
+/* ---------- Popup Translation Bubble ---------- */
+#sk_bubble {
+  background-color: var(--bg) !important;
+  color: var(--fg) !important;
+  border-color: var(--border) !important;
+}
+
+#sk_bubble * {
+  color: var(--fg) !important;
+}
+
+#sk_bubble div.sk_arrow div:nth-of-type(1) {
+  border-top-color: var(--border) !important;
+  border-bottom-color: var(--border) !important;
+}
+
+#sk_bubble div.sk_arrow div:nth-of-type(2) {
+  border-top-color: var(--bg) !important;
+  border-bottom-color: var(--bg) !important;
+}
+
+/* ---------- Search ---------- */
+#sk_status,
+#sk_find {
+  font-size: var(--font-size);
+  border-color: var(--border);
+}
+
+.sk_theme kbd {
+  background: var(--bg-dark);
+  border-color: var(--border);
+  box-shadow: none;
+  color: var(--fg);
+}
+
+.sk_theme .feature_name span {
+  color: var(--main-fg);
+}
+
+/* ---------- ACE Editor ---------- */
+#sk_editor {
+  background: var(--bg-dark) !important;
+  height: 50% !important;
+  /* Remove this to restore the default editor size */
+}
+
+.ace_dialog-bottom {
+  border-top: 1px solid var(--bg) !important;
+}
+
+.ace-chrome .ace_print-margin,
+.ace_gutter,
+.ace_gutter-cell,
+.ace_dialog {
+  background: var(--bg) !important;
+}
+
+.ace-chrome {
+  color: var(--fg) !important;
+}
+
+.ace_gutter,
+.ace_dialog {
+  color: var(--fg) !important;
+}
+
+.ace_cursor {
+  color: var(--fg) !important;
+}
+
+.normal-mode .ace_cursor {
+  background-color: var(--fg) !important;
+  border: var(--fg) !important;
+  opacity: 0.7 !important;
+}
+
+.ace_marker-layer .ace_selection {
+  background: var(--select) !important;
+}
+
+.ace_editor,
+.ace_dialog span,
+.ace_dialog input {
+  font-family: var(--font);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
+}
+`;
---
+++ firefox/user.js
@@ -0,0 +1,344 @@
+//
+/* You may copy+paste this file and use it as it is.
+ *
+ * If you make changes to your about:config while the program is running, the
+ * changes will be overwritten by the user.js when the application restarts.
+ *
+ * To make lasting changes to preferences, you will have to edit the user.js.
+ */
+
+/****************************************************************************
+ * Betterfox                                                                *
+ * "Ad meliora"                                                             *
+ * version: 126                                                             *
+ * url: https://github.com/yokoffing/Betterfox                              *
+****************************************************************************/
+
+/****************************************************************************
+ * SECTION: FASTFOX                                                         *
+****************************************************************************/
+/** GENERAL ***/
+user_pref("content.notify.interval", 100000);
+
+/** GFX ***/
+user_pref("gfx.canvas.accelerated.cache-items", 4096);
+user_pref("gfx.canvas.accelerated.cache-size", 512);
+user_pref("gfx.content.skia-font-cache-size", 20);
+
+/** DISK CACHE ***/
+user_pref("browser.cache.jsbc_compression_level", 3);
+
+/** MEDIA CACHE ***/
+user_pref("media.memory_cache_max_size", 65536);
+user_pref("media.cache_readahead_limit", 7200);
+user_pref("media.cache_resume_threshold", 3600);
+
+/** IMAGE CACHE ***/
+user_pref("image.mem.decode_bytes_at_a_time", 32768);
+
+/** NETWORK ***/
+user_pref("network.http.max-connections", 1800);
+user_pref("network.http.max-persistent-connections-per-server", 10);
+user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5);
+user_pref("network.http.pacing.requests.enabled", false);
+user_pref("network.dnsCacheExpiration", 3600);
+user_pref("network.ssl_tokens_cache_capacity", 10240);
+
+/** SPECULATIVE LOADING ***/
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.dns.disablePrefetchFromHTTPS", true);
+user_pref("network.prefetch-next", false);
+user_pref("network.predictor.enabled", false);
+user_pref("network.predictor.enable-prefetch", false);
+
+/** EXPERIMENTAL ***/
+user_pref("layout.css.grid-template-masonry-value.enabled", true);
+user_pref("dom.enable_web_task_scheduling", true);
+user_pref("dom.security.sanitizer.enabled", true);
+
+/****************************************************************************
+ * SECTION: SECUREFOX                                                       *
+****************************************************************************/
+/** TRACKING PROTECTION ***/
+user_pref("browser.contentblocking.category", "strict");
+user_pref("urlclassifier.trackingSkipURLs", "*.reddit.com, *.twitter.com, *.twimg.com, *.tiktok.com");
+user_pref("urlclassifier.features.socialtracking.skipURLs", "*.instagram.com, *.twitter.com, *.twimg.com");
+user_pref("network.cookie.sameSite.noneRequiresSecure", true);
+user_pref("browser.download.start_downloads_in_tmp_dir", true);
+user_pref("browser.helperApps.deleteTempFileOnExit", true);
+user_pref("browser.uitour.enabled", false);
+user_pref("privacy.globalprivacycontrol.enabled", true);
+
+/** OCSP & CERTS / HPKP ***/
+user_pref("security.OCSP.enabled", 0);
+user_pref("security.remote_settings.crlite_filters.enabled", true);
+user_pref("security.pki.crlite_mode", 2);
+
+/** SSL / TLS ***/
+user_pref("security.ssl.treat_unsafe_negotiation_as_broken", true);
+user_pref("browser.xul.error_pages.expert_bad_cert", true);
+user_pref("security.tls.enable_0rtt_data", false);
+
+/** DISK AVOIDANCE ***/
+user_pref("browser.privatebrowsing.forceMediaMemoryCache", true);
+user_pref("browser.sessionstore.interval", 60000);
+
+/** SHUTDOWN & SANITIZING ***/
+user_pref("privacy.history.custom", true);
+
+/** SEARCH / URL BAR ***/
+user_pref("browser.search.separatePrivateDefault.ui.enabled", true);
+user_pref("browser.urlbar.update2.engineAliasRefresh", true);
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
+user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false);
+user_pref("browser.formfill.enable", false);
+user_pref("security.insecure_connection_text.enabled", true);
+user_pref("security.insecure_connection_text.pbmode.enabled", true);
+user_pref("network.IDN_show_punycode", true);
+
+/** HTTPS-FIRST POLICY ***/
+user_pref("dom.security.https_first", true);
+user_pref("dom.security.https_first_schemeless", true);
+
+/** PASSWORDS ***/
+user_pref("signon.formlessCapture.enabled", false);
+user_pref("signon.privateBrowsingCapture.enabled", false);
+user_pref("network.auth.subresource-http-auth-allow", 1);
+user_pref("editor.truncate_user_pastes", false);
+
+/** MIXED CONTENT + CROSS-SITE ***/
+user_pref("security.mixed_content.block_display_content", true);
+user_pref("security.mixed_content.upgrade_display_content", true);
+user_pref("security.mixed_content.upgrade_display_content.image", true);
+user_pref("pdfjs.enableScripting", false);
+user_pref("extensions.postDownloadThirdPartyPrompt", false);
+
+/** HEADERS / REFERERS ***/
+user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
+
+/** CONTAINERS ***/
+user_pref("privacy.userContext.ui.enabled", true);
+
+/** WEBRTC ***/
+user_pref("media.peerconnection.ice.proxy_only_if_behind_proxy", true);
+user_pref("media.peerconnection.ice.default_address_only", true);
+
+/** SAFE BROWSING ***/
+user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+
+/** MOZILLA ***/
+user_pref("permissions.default.desktop-notification", 2);
+user_pref("permissions.default.geo", 2);
+user_pref("geo.provider.network.url", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
+user_pref("permissions.manager.defaultsUrl", "");
+user_pref("webchannel.allowObject.urlWhitelist", "");
+
+/** TELEMETRY ***/
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.server", "data:,");
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.coverage.opt-out", true);
+user_pref("toolkit.coverage.opt-out", true);
+user_pref("toolkit.coverage.endpoint.base", "");
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+
+/** EXPERIMENTS ***/
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("app.normandy.enabled", false);
+user_pref("app.normandy.api_url", "");
+
+/** CRASH REPORTS ***/
+user_pref("breakpad.reportURL", "");
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
+
+/** DETECTION ***/
+user_pref("captivedetect.canonicalURL", "");
+user_pref("network.captive-portal-service.enabled", false);
+user_pref("network.connectivity-service.enabled", false);
+
+/****************************************************************************
+ * SECTION: PESKYFOX                                                        *
+****************************************************************************/
+/** MOZILLA UI ***/
+user_pref("browser.privatebrowsing.vpnpromourl", "");
+user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+user_pref("browser.discovery.enabled", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+user_pref("browser.preferences.moreFromMozilla", false);
+user_pref("browser.tabs.tabmanager.enabled", false);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.aboutwelcome.enabled", false);
+
+/** THEME ADJUSTMENTS ***/
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.compactmode.show", true);
+user_pref("browser.display.focus_ring_on_anything", true);
+user_pref("browser.display.focus_ring_style", 0);
+user_pref("browser.display.focus_ring_width", 0);
+user_pref("layout.css.prefers-color-scheme.content-override", 2);
+user_pref("browser.privateWindowSeparation.enabled", false); // WINDOWS
+
+/** COOKIE BANNER HANDLING ***/
+user_pref("cookiebanners.service.mode", 1);
+user_pref("cookiebanners.service.mode.privateBrowsing", 1);
+
+/** FULLSCREEN NOTICE ***/
+user_pref("full-screen-api.transition-duration.enter", "0 0");
+user_pref("full-screen-api.transition-duration.leave", "0 0");
+user_pref("full-screen-api.warning.delay", -1);
+user_pref("full-screen-api.warning.timeout", 0);
+
+/** URL BAR ***/
+user_pref("browser.urlbar.suggest.calculator", true);
+user_pref("browser.urlbar.unitConversion.enabled", true);
+user_pref("browser.urlbar.trending.featureGate", false);
+
+/** NEW TAB PAGE ***/
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+
+/** POCKET ***/
+user_pref("extensions.pocket.enabled", false);
+
+/** DOWNLOADS ***/
+user_pref("browser.download.always_ask_before_handling_new_types", true);
+user_pref("browser.download.manager.addToRecentDocs", false);
+
+/** PDF ***/
+user_pref("browser.download.open_pdf_attachments_inline", true);
+
+/** TAB BEHAVIOR ***/
+user_pref("browser.bookmarks.openInTabClosesMenu", false);
+user_pref("browser.menu.showViewImageInfo", true);
+user_pref("findbar.highlightAll", true);
+user_pref("layout.word_select.eat_space_to_next_word", false);
+
+/****************************************************************************
+ * START: MY OVERRIDES                                                      *
+****************************************************************************/
+// visit https://github.com/yokoffing/Betterfox/wiki/Common-Overrides
+// visit https://github.com/yokoffing/Betterfox/wiki/Optional-Hardening
+// Enter your personal overrides below this line:
+user_pref("extensions.formautofill.addresses.enabled", true);
+user_pref("identity.fxaccounts.enabled", false);
+
+/****************************************************************************
+ * SECTION: SMOOTHFOX                                                       *
+****************************************************************************/
+
+///  NATURAL SMOOTH SCROLLING V4 "SHARP" - AveYo, 2020-2022             preset     [default]
+///  copy into firefox/librewolf profile as user.js, add to existing, or set in about:config
+user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 120);//NSS    [120]
+user_pref("general.smoothScroll.msdPhysics.enabled", true);//NSS  [false]
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 500);//NSS   [1250]
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 400);//NSS   [1000]
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 12);//NSS     [12]
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", "1.3");//NSS    [1.3]
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 1000);//NSS   [2000]
+user_pref("general.smoothScroll.currentVelocityWeighting", "0.5");//NSS ["0.25"]
+user_pref("general.smoothScroll.stopDecelerationWeighting", "0.8");//NSS  ["0.4"]
+
+/// adjust multiply factor for mousewheel - or set to false if scrolling is way too fast  
+user_pref("mousewheel.system_scroll_override.horizontal.factor", 200);//NSS    [200]
+user_pref("mousewheel.system_scroll_override.vertical.factor", 200);//NSS    [200]
+user_pref("mousewheel.system_scroll_override_on_root_content.enabled", true);//NSS   [true]
+user_pref("mousewheel.system_scroll_override.enabled", true);//NSS   [true]
+
+/// adjust pixels at a time count for mousewheel - cant do more than a page at once if <100
+user_pref("mousewheel.default.delta_multiplier_x", 100);//NSS    [100]
+user_pref("mousewheel.default.delta_multiplier_y", 100);//NSS    [100]
+user_pref("mousewheel.default.delta_multiplier_z", 100);//NSS    [100]
+
+///  this preset will reset couple extra variables for consistency
+user_pref("apz.allow_zooming", true);//NSS   [true]
+user_pref("apz.force_disable_desktop_zooming_scrollbars", false);//NSS  [false]
+user_pref("apz.paint_skipping.enabled", true);//NSS   [true]
+user_pref("apz.windows.use_direct_manipulation", true);//NSS   [true]
+user_pref("dom.event.wheel-deltaMode-lines.always-disabled", false);//NSS  [false]
+user_pref("general.smoothScroll.durationToIntervalRatio", 200);//NSS    [200]
+user_pref("general.smoothScroll.lines.durationMaxMS", 150);//NSS    [150]
+user_pref("general.smoothScroll.lines.durationMinMS", 150);//NSS    [150]
+user_pref("general.smoothScroll.other.durationMaxMS", 150);//NSS    [150]
+user_pref("general.smoothScroll.other.durationMinMS", 150);//NSS    [150]
+user_pref("general.smoothScroll.pages.durationMaxMS", 150);//NSS    [150]
+user_pref("general.smoothScroll.pages.durationMinMS", 150);//NSS    [150]
+user_pref("general.smoothScroll.pixels.durationMaxMS", 150);//NSS    [150]
+user_pref("general.smoothScroll.pixels.durationMinMS", 150);//NSS    [150]
+user_pref("general.smoothScroll.scrollbars.durationMaxMS", 150);//NSS    [150]
+user_pref("general.smoothScroll.scrollbars.durationMinMS", 150);//NSS    [150]
+user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 200);//NSS    [200]
+user_pref("general.smoothScroll.mouseWheel.durationMinMS", 50);//NSS     [50]
+user_pref("layers.async-pan-zoom.enabled", true);//NSS   [true]
+user_pref("layout.css.scroll-behavior.spring-constant", "250");//NSS    [250]
+user_pref("mousewheel.transaction.timeout", 1500);//NSS   [1500]
+user_pref("mousewheel.acceleration.factor", 10);//NSS     [10]
+user_pref("mousewheel.acceleration.start", -1);//NSS     [-1]
+user_pref("mousewheel.min_line_scroll_amount", 5);//NSS      [5]
+user_pref("toolkit.scrollbox.horizontalScrollDistance", 4);//NSS      [5]
+user_pref("toolkit.scrollbox.verticalScrollDistance", 3);//NSS      [3]
+///
+
+/****************************************************************************
+ * END: BETTERFOX                                                           *
+****************************************************************************/
+
+
+/****************************************************************************
+ * PotatoFox https://codeberg.org/awwpotato/PotatoFox                       *
+****************************************************************************/
+
+// userchrome.css usercontent.css activate
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Fill SVG Color
+user_pref("svg.context-properties.content.enabled", true);
+
+// CSS's `:has()` selector 
+user_pref("layout.css.has-selector.enabled", true);
+
+// Integrated calculator at urlbar
+user_pref("browser.urlbar.suggest.calculator", true);
+
+// Integrated unit convertor at urlbar
+user_pref("browser.urlbar.unitConversion.enabled", true);
+
+// Trim  URL
+user_pref("browser.urlbar.trimHttps", true);
+user_pref("browser.urlbar.trimURLs", true);
+
+// GTK rounded corners
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
+
+// show compact mode
+user_pref("browser.compactmode.show", true);
+
+// fix sidebar tab drag on linux
+user_pref("widget.gtk.ignore-bogus-leave-notify", 1);
+
+// option for transparent tab bg (disabled to due issues on some websites)
+// user_pref("browser.tabs.allow_transparent_browser", false);
+
+// uidensity -> compact
+user_pref("browser.uidensity", 1);
+
+
+/****************************************************************************
+ * MY STUFF
+****************************************************************************/
+// Disable alt key opening menu
+user_pref("ui.key.menuAccessKeyFocuses, false");
+user_pref("ui.key.menuAccessKey", 0);
