3c01d67a4d6bc4c6acabf41c642c986418c82808
William Blazer <82427064+wblazer@users.noreply.github.com>
2024-10-18 17:40:08 -0500

    Updates

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@ DOTFILES_DIR="$HOME/.dotfiles"
 
 # Run individual install scripts
 bash "$DOTFILES_DIR/terminal/install.sh"
-# Add other install scripts as needed, e.g.:
-# bash "$DOTFILES_DIR/vim/install.sh"
-# bash "$DOTFILES_DIR/git/install.sh"
+bash "$DOTFILES_DIR/zed/install.sh"
+bash "$DOTFILES_DIR/git/install.sh"
 
 echo "All installations complete."
--- terminal/install.sh
+++ terminal/install.sh
@@ -4,14 +4,17 @@ DOTFILES_DIR="$HOME/.dotfiles/terminal"
 CONFIG_DIR="$HOME/.config"
 
 create_symlink() {
-    local src="$1"
-    local dest="$2"
-    if [ -e "$dest" ]; then
-        echo "Warning: $dest already exists. Skipping..."
-    else
-        ln -s "$src" "$dest" && echo "Created symlink: $dest -> $src"
-    fi
+  local src="$1"
+  local dest="$2"
+  if [ -e "$dest" ]; then
+    echo "Warning: $dest already exists. Skipping..."
+  else
+    ln -s "$src" "$dest" && echo "Created symlink: $dest -> $src"
+  fi
 }
+
+mkdir -p ~/.config/tmux/plugins/catppuccin
+git clone https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
 
 mkdir -p "$CONFIG_DIR/alacritty" "$CONFIG_DIR/tmux"
 
