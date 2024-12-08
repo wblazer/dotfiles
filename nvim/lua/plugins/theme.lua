c490cb3d91433cd707892168b646667cdc7df6d7
William Blazer <82427064+wblazer@users.noreply.github.com>
2024-12-08 11:27:28 -0600

    Lots of changes

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -8,6 +8,21 @@ return {
     },
   },
   {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    name = "gruvbox-material",
+    config = true,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "moon",
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
