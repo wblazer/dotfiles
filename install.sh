a613f4f9f9a54475317dc50a3216ab2f2861dfdd
William Blazer <82427064+wblazer@users.noreply.github.com>
2025-01-14 13:34:50 -0600

    Added backgrounds

--- install.sh
+++ install.sh
@@ -9,4 +9,6 @@ bash "$DOTFILES_DIR/terminal/install.sh"
 bash "$DOTFILES_DIR/zed/install.sh"
 bash "$DOTFILES_DIR/git/install.sh"
 
+ln -s "$DOTFILES_DIR/backgrounds" "$HOME/.local/share/backgrounds"
+
 echo "All installations complete."
