e00b5fcd0ff37a143e1d8f7be21b9c3049ba62f4
William Blazer <82427064+wblazer@users.noreply.github.com>
2025-01-14 13:01:13 -0600

    Add zed to theme-switcher

--- scripts/theme-switcher.sh
+++ scripts/theme-switcher.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-THEMES=("Gruvbox Material" "Catppuccin")
-THEME=$(gum choose "${THEMES[@]}" --header "Choose your theme" --height 10 | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
-DOTFILES="$HOME/.dotfiles" # adjust path as needed
+THEMES=("Gruvbox Material" "Catppuccin Macchiato")
+THEME_PROPER=$(gum choose "${THEMES[@]}" --header "pick theme")
+THEME=$(echo "$THEME_PROPER" | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
+DOTFILES="$HOME/.dotfiles"
 
-if [ -n "$THEME" ] && [ "$THEME" != "<<-back" ]; then
+if [ -n "$THEME" ]; then
+  ### Ghostty
   ln -sf "$DOTFILES/themes/ghostty/$THEME" "$DOTFILES/themes/ghostty/config"
+
+  ### Zed
+  # create temp file
+  tmp=$(mktemp)
+
+  # set theme
+  jq --arg theme "$THEME_PROPER" '.theme.dark = $theme' "$DOTFILES/zed/settings.json" > "$tmp"
+
+  # apply overrides if they exist
+  OVERRIDE_PATH="$DOTFILES/themes/zed/$THEME.json"
+  if [ -f "$OVERRIDE_PATH" ]; then
+    overrides=$(jq -r '.["experimental.theme_overrides"]' "$OVERRIDE_PATH")
+    jq --argjson overrides "$overrides" '.["experimental.theme_overrides"] = $overrides' "$tmp" > "$tmp.2" && mv "$tmp.2" "$tmp"
+  else
+    echo "no zed theme overrides found"
+    jq 'del(.["experimental.theme_overrides"])' "$tmp" > "$tmp.2" && mv "$tmp.2" "$tmp"
+  fi
+
+  # move temp file back
+  mv "$tmp" "$DOTFILES/zed/settings.json"
 fi
